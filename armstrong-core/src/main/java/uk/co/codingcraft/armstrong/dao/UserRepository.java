27f024c5274a1b9bc064eb0bba9873cf282cf56c
David Pashley <david@davidpashley.com>
2011-12-04 17:16:08 +0000

    Create a UserRepository

---
+++ armstrong-core/src/main/java/uk/co/codingcraft/armstrong/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package uk.co.codingcraft.armstrong.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import uk.co.codingcraft.armstrong.model.User;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+
+}
